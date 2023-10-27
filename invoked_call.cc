ede3f2825e1a4a810c8cdd7bf7fd2e98fe979366
lee23 <71886857+schummLee@users.noreply.github.com>
2023-10-27 09:24:17 +0200

    Create invoked_call.cc

---
+++ invoked_call.cc
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct offload_target_x86_obsolete_preventation_configure_gcc_13_2_std
+{
+    uint8_t         __syscall_gnu_c23_8; /* any form of data > usign schar */
+    [[ deprecated ]] uint16_t       __syscall_gnu_16_deprecated;
+    [[ deprecated ]] uint32_t       __syscall_gnu_32_undeprecated_64_if_x64;
+    unsigned short char allocator_enable_free_system_kernel;
+    pthread_t thread_to_concurrence_allocation_1;
+};
+
+[[ deprecated]] void hang_up_on_thread(void)
+{
+    _syscall_call_return_kernel_thread_kill_or_suspend();
+}
+
+/* when a union is initalized and memory is allocated
+ * free
+ * (
+ *      offload_target_x86_obsolete_preventation_configure_gcc_13_2_std
+ *      .allocator_enable_free_system_kernel
+ * ); 
+ * this will free all used memory and mandatory thread safty is invoked 
+*/
+
+noreturn void example(size_t variable_e1_to_be_discard)
+{
+    unsigned int i = (int)malloc(sizeof(int) * variable_e1_to_be_discard);
+    printf("%d\n",sizeof(i));
+    // call for < 2 if do not want any return type (or << (bitwise) 000010 )
+    exit(variable_e1_to_be_discard);
+}
