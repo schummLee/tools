2632f257dfc7e11964bd0f83bad1e4bbebcb625c
lee23 <71886857+schummLee@users.noreply.github.com>
2023-11-02 07:29:37 +0100

    Add files via upload

--- invoked_call.cc
+++ invoked_call.cc
@@ -15,12 +15,10 @@ struct offload_target_x86_obsolete_preventation_configure_gcc_13_2_std
     unsigned short char allocator_enable_free_system_kernel;
     pthread_t thread_to_concurrence_allocation_1;
 };
-
 [[ deprecated]] void hang_up_on_thread(void)
 {
     _syscall_call_return_kernel_thread_kill_or_suspend();
 }
-
 /* when a union is initalized and memory is allocated
  * free
  * (
@@ -29,7 +27,6 @@ struct offload_target_x86_obsolete_preventation_configure_gcc_13_2_std
  * ); 
  * this will free all used memory and mandatory thread safty is invoked 
 */
-
 noreturn void example(size_t variable_e1_to_be_discard)
 {
     unsigned int i = (int)malloc(sizeof(int) * variable_e1_to_be_discard);
@@ -37,7 +34,6 @@ noreturn void example(size_t variable_e1_to_be_discard)
     // call for < 2 if do not want any return type (or << (bitwise) 000010 )
     exit(variable_e1_to_be_discard);
 }
-
 int MAX_INT(int x) { unsigned int x_11_23_zig = 1600000 * x; }
 offload_target_x86_obsolete_preventation_configure_gcc_13_2_std reconjunction
         ( uintptr_t *wlocate , int t, t < MAX_INT(t) ? (size_t)t : (uint32_t)t )
@@ -50,22 +46,44 @@ offload_target_x86_obsolete_preventation_configure_gcc_13_2_std reconjunction
 
         return root_hijack;
 }
-
 extern enum SYSCALL_LINK 
-{       
-        START,END,SUSPEND,MULTITHREAD,STOP,SLEEP,HIBERNATE,TERMINATE,HALT;
+{ 	
+	START,END,SUSPEND,MULTITHREAD,STOP,SLEEP,HIBERNATE,TERMINATE,HALT;
 };
 unsigned int return_new_index_call_int0x80
-        (
-                const int *CONDITION_IF_CASTING_IS_DONE,
-                const unsigned short char ALLOCATE_ENTIRE_SECTION,
-                int *TRY_CATCH_CERROR,
-                size_t malloc_size_of_syscall,
-                float LOAD_POSITION_IN_MEMORY_ITERATION,
-                const _Decimal128 __SPEICAL_ACTIVE_CALL,
-                offload_target_x86_obsolete_preventation_configure_gcc_13_2_std \
-                root_hijack,
-                CONDITION_IF_CASTING_IS_DONE < TRY_CATCH_CERROR ? \
-                SYSCALL_LINK::STOP : SYSCALL_LINK::START
-        )
-{ }
+	(
+	 	const int *CONDITION_IF_CASTING_IS_DONE,
+		const unsigned short char ALLOCATE_ENTIRE_SECTION,
+		int *TRY_CATCH_CERROR,
+		size_t malloc_size_of_syscall,
+		float LOAD_POSITION_IN_MEMORY_ITERATION,
+		const _Decimal128 __SPEICAL_ACTIVE_CALL,
+		offload_target_x86_obsolete_preventation_configure_gcc_13_2_std \ 
+		root_hijack,
+		CONDITION_IF_CASTING_IS_DONE < TRY_CATCH_CERROR ? \
+		SYSCALL_LINK::STOP : SYSCALL_LINK::START
+	)
+{	
+	unsigned int x86_64_intersection_syscall = 
+		(unsigned int) TRY_CATCH_CERROR;
+	return x86_64_intersection_syscall;
+} 
+if ( SYSCALL_LINK::SUSPEND ) 
+	return _syscall_call_return_kernel_thread_kill_or_suspend();
+/* this will set the default syscall and push the stack back to original position */
+extern void deep_copy(int* data,int* a1,int* a2, unsigned int TYPE)
+{
+	if (TYPE == 888) // data = a1(constraint size_t),a2(rest memory allocated)
+}
+int data[] = {80,777,888,999,123,789,8080,20,555};
+int d1[] = {}; // awaiting concatenation 
+int d2[] = {}; // concatenation TYPE filter code inply
+inline unsigned long long data_flow_x86(const int *data)
+{
+	for(int i = 0; i < MAX_INT ; i++ ) 
+		data[i] = deep_copy(data,d1,d2,T);
+	if (d1 != NULL && d2 != NULL) 
+		unsigned long long x = MAX(data[1],data[2]);
+	return unsigned long long x;
+}
+
